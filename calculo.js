85704363fd1e55ac52d7e0fb627df6fa9f18f3ac
brunojyyh <brunojyyh@hotmail.com>
2021-07-13 19:05:22 -0300

    version 0.2

--- calculo.js
+++ calculo.js
@@ -3,7 +3,11 @@ function calcular(){
     let salario1 = Number(salario.value)
     var resultado = document.getElementById("resultado");
     if(salario.value.length > 0) {
-        resultado.innerHTML =  `o seu salario é: ${salario1 + 5000}`
+        if (salario1 <= 1100) {
+            resultado.innerHTML = ` A aliquota de inss  para o seu salario é 7,5% <br> <br> o salario liquido = ${salario1 - (salario1 * 0.075) }`
+        } else if (salario1 >= 1100.01 || salario1 <= 2203.48) {
+            resultado.innerHTML = `A aliquota de inss  para o seu salario é 9% <br> <br> Salario liquido = ${salario1 - (salario1 * 0.09)} `
+        }  
     }else {
         window.alert('salario nao informado')
     }
